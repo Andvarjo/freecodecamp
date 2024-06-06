0d38b8354636d88e0e98a071acfddc9068235128
andvarjo <afvj92@gmail.com>
2024-06-06 15:46:07 -0500

    world cup DB finished

--- SQL/world_cup/insert_data.sh
+++ SQL/world_cup/insert_data.sh
@@ -58,7 +58,7 @@ do
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
     
     #INSERT DATA
-    echo $YEAR $ROUND $WINNER_ID $OPPONENT_ID $WINNER_GOALS $OPPONENT_GOALS
+    
     INSERT_GAME=$($PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) VALUES ($YEAR,'$ROUND',$WINNER_ID,$OPPONENT_ID,$WINNER_GOALS,$OPPONENT_GOALS)")
   fi
 
